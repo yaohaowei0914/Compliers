fa73112823a8bbc1f55d20668bee99eda4e96e15
yaohaowei0914 <973396597@qq.com>
2024-09-16 16:26:32 +0800

    master

---
+++ main2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, i, t, n;
+
+    // 初始化
+    a = 0;
+    b = 1;
+    i = 1;
+
+    // 输入要生成的斐波那契数列的项数
+    scanf("%d", &n);
+
+    // 输出前两个斐波那契数
+    printf("%d\n", a);
+    printf("%d\n", b);
+
+    // 循环输出剩余的斐波那契数
+    while (i < n) {
+        t = b;
+        b = a + b;
+        printf("%d\n", b);
+        a = t;
+        i = i + 1;
+    }
+
+    return 0;
+}
